e4dc05cde3b51995d5f033bc8fef9d15e4d36f51
Tyrone DeSilva <tdesilvaqihao@gmail.com>
2021-06-01 16:29:39 -0700

    implemented nn rk4 lkv

--- lkv.jl
+++ lkv.jl
@@ -1,6 +1,22 @@
 using DifferentialEquations
 
 
+function sample_interval(start, stop, n)
+    result = Vector{Float64}(undef, n)
+    hm = Set{Float64}()
+    for i in 1:n
+        while true
+            val = start + rand() * (stop - start)
+            (val in hm) && continue
+            result[i] = val
+            push!(hm, val)
+            break
+        end
+    end
+    return result
+end
+
+
 function lotka_volterra!(dx, x, p, t)
     α, β, δ, γ = p
     dx[1] = α * x[1] - β * x[1] * x[2]
@@ -10,7 +26,7 @@ end
 
 function sample_lotka_volterra(x₀, n, T, α, β, δ, γ; train=false)
     if train
-        sample_points = rand(Float64, n) * T
+        sample_points = sample_interval(0, T, n)
     else
         sample_points = LinRange(0, T, n)
     end
--- train_rk4.jl
+++ train_rk4.jl
@@ -1,3 +1,4 @@
+## Create datasets, loaders
 using CUDA, Flux, Plots, Statistics
 using Flux.Optimise:update!
 using Flux.Data:DataLoader
@@ -8,7 +9,7 @@ include("lkv.jl")
 
 lkv_ic = [0.8, 0.4]
 true_params = [1.1, 0.4, 0.1, 0.4]
-lkv_train_samples = sample_lotka_volterra(lkv_ic, 200, 100., true_params...; train=true)
+lkv_train_samples = sample_lotka_volterra(lkv_ic, 500, 100, true_params...; train=true)
 lkv_test_samples = sample_lotka_volterra(lkv_ic, 500, 100, true_params...)
 
 lkv_train_tuple = (
@@ -20,6 +21,7 @@ lkv_train_tuple = (
 train_loader = DataLoader(lkv_train_tuple; batchsize=16, shuffle=true)
 test_loader = DataLoader(lkv_train_samples; batchsize=32, shuffle=false)
 
+## Train rk4 model to learn lkv params
 est_params = rand(4)
 println(est_params)
 θ = Flux.params(est_params)
@@ -29,6 +31,7 @@ f(x, α, β, δ, γ) = hcat(
     δ .* x[:, 1] .* x[:, 2] .- γ .* x[:, 2]
 )
 
+# maybe the arg expansion somehow forces grad to return tuple?
 f(t, x) = f(x, est_params...)
 
 opt = Descent(0.0001)
@@ -65,6 +68,65 @@ plot!(
     label=["actual x₁" "actual x₂"]
 )
 lkv_est_samples = sample_lotka_volterra(lkv_ic, 500, 100, est_params...)
+plot!(
+    p1,
+    lkv_est_samples.t,
+    transpose(lkv_est_samples),
+    label=["estimated x₁" "estimated x₂"]
+)
+
+## Train rk4 with fully connected network
+fc = Chain(
+    Dense(2, 16, relu),
+    Dense(16, 32, relu),
+    Dense(32, 16, relu),
+    Dense(16, 2)
+)
+θ = params(fc)
+f(t, x) = fc(x')'
+
+opt = Descent(0.0001)
+predict(t, x) = rk4(f, t, x)
+
+# TODO get CUDA working
+for i in 1:10
+    for (x₀, y, T) in train_loader
+        t = hcat(LinRange.(0, T, 100)...)
+        grad = gradient(θ) do 
+            loss = Flux.Losses.mse(predict(t, x₀), y)
+            return loss
+        end
+        # TODO this time it's all matrix/arrays...
+        update!(opt, θ, grad)
+    end
+end
+
+p1 = plot(
+    lkv_train_samples.t,
+    transpose(lkv_train_samples),
+    title="LKV Model",
+    label=["x₁ train" "x₂ train"],
+    seriestype=:scatter
+)
+plot!(
+    p1,
+    lkv_test_samples.t,
+    transpose(lkv_test_samples),
+    label=["actual x₁" "actual x₂"]
+)
+
+function nn_lotka_volterra!(dx, x, p, t)
+    dx[1], dx[2] = fc(x)
+end
+
+function sample_nn_lotka_volterra(x₀, n, T)
+    sample_points = LinRange(0, T, n)
+    tspan = (0.0, T)
+    prob = ODEProblem(nn_lotka_volterra!, x₀, tspan, undef)
+    return solve(prob, saveat=sample_points)
+end
+
+lkv_est_samples = sample_nn_lotka_volterra(lkv_ic, 500, 100)
 plot!(
     p1,
     lkv_est_samples.t,
