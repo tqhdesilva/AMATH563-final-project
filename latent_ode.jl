8e2de2eefd0ce81a644be3eb623d088c0ec1c059
Tyrone DeSilva <tdesilvaqihao@gmail.com>
2021-06-06 09:29:21 -0700

    added cpu/gpu code

--- latent_ode.jl
+++ latent_ode.jl
@@ -1,19 +1,19 @@
-using DifferentialEquations, Flux, DiffEqFlux
+using DifferentialEquations, Flux, DiffEqFlux, CUDA
 using Flux.Losses:logitbinarycrossentropy
+import Flux:gpu, cpu
 
 
 struct Encoder
     zdim::Int
-    xdim::Int
-    hdim::Int
     rnn_network::Chain
-    function Encoder(zdim::Int, xdim::Int; hdim::Int=32)
-        rnn_network = Chain(RNN(xdim, hdim), Dense(hdim, zdim * 2))
-        new(zdim, xdim, hdim, rnn_network)
-    end
 end
 
-function (e::Encoder)(x::Matrix{Float32})
+function new_encoder(zdim::Int, xdim::Int; hdim::Int=32)
+    rnn_network = Chain(RNN(xdim, hdim), Dense(hdim, zdim * 2))
+    return Encoder(zdim, rnn_network)
+end
+
+function (e::Encoder)(x::AbstractArray{Float32,2})
     # x is matrix of xdim x tsteps
     x = reverse(Flux.unstack(x, 2))
     Flux.reset!(e.rnn_network)
@@ -22,6 +22,7 @@ function (e::Encoder)(x::Matrix{Float32})
     logσ = out[e.zdim + 1:end]
     return μ, logσ
 end
+
 
 function (e::Encoder)(x::Array{Float32,3})
     # x is a array of xdim x tsteps x batchsize
@@ -34,87 +35,87 @@ function (e::Encoder)(x::Array{Float32,3})
 end
 
 struct Decoder
-    zdim::Int
-    xdim::Int
     network::Chain
-    function Decoder(zdim::Int, xdim::Int)
-        network = Chain(
-            Dense(zdim, 32, relu),
-            Dense(32, 64, relu),
-            Dense(64, 64, relu),
-            Dense(64, xdim)
-        )
-        new(zdim, xdim, network)
-    end
 end
 
-function (d::Decoder)(z::Union{Vector{Float32},Matrix{Float32}})
+function new_decoder(zdim::Int, xdim::Int)
+    network = Chain(
+        Dense(zdim, 32, relu),
+        Dense(32, 32, relu),
+        Dense(32, 32, relu),
+        Dense(32, xdim)
+    )
+    return Decoder(network)
+end
+
+function (d::Decoder)(z::Union{AbstractArray{Float32,1},AbstractArray{Float32,2}})
     return d.network(z)
 end
 
 struct VAE
-    xdim::Int
-    zdim::Int
-    hdim::Int
     encoder::Encoder
     decoder::Decoder
-    function VAE(xdim::Int, zdim::Int, hdim::Int)
-        encoder = Encoder(zdim, xdim; hdim=hdim)
-        decoder = Decoder(zdim, xdim)
-        new(xdim, zdim, hdim, encoder, decoder)
-    end
+end
+
+function new_vae(xdim::Int, zdim::Int, hdim::Int)
+    encoder = new_encoder(zdim, xdim; hdim=hdim)
+    decoder = new_decoder(zdim, xdim)
+    return VAE(encoder, decoder)
 end
 
 struct LatentODE
     dzdt::Chain
     vae::VAE
     β::AbstractFloat
-    function LatentODE(xdim::Int, zdim::Int, hdim::Int, β::AbstractFloat=1.0)
-        dzdt = Chain(
-            Dense(zdim, 32, relu),
-            Dense(32, 32, relu),
-            Dense(32, zdim)
-        )
-        vae = VAE(xdim, zdim, hdim)
-        new(dzdt, vae, β)
-    end
 end
 
+function new_latent_ode(xdim::Int, zdim::Int, hdim::Int; β::AbstractFloat=1.0)::LatentODE
+    dzdt = Chain(
+        Dense(zdim, 32, relu),
+        Dense(32, 32, relu),
+        Dense(32, 32, relu),
+        Dense(32, zdim)
+    )
+    vae = new_vae(xdim, zdim, hdim)
+    return LatentODE(dzdt, vae, β)
+end
+
+gpu(enc::Encoder) = Encoder(enc.zdim, enc.rnn_network |> gpu)
+cpu(enc::Encoder) = Encoder(enc.zdim, enc.rnn_network |> cpu)
+gpu(dec::Decoder) = Decoder(dec.network |> gpu)
+cpu(dec::Decoder) = Decoder(dec.network |> cpu)
+gpu(vae::VAE) = VAE(vae.encoder |> gpu, vae.decoder |> gpu)
+cpu(vae::VAE) = VAE(vae.encoder |> cpu, vae.decoder |> cpu)
+gpu(lode::LatentODE) = LatentODE(lode.dzdt |> gpu, lode.vae |> gpu, lode.β)
+cpu(lode::LatentODE) = LatentODE(lode.dzdt |> cpu, lode.vae |> cpu, lode.β)
 
 function (l::LatentODE)(
-    x::Matrix{Float32},
-    t::Vector{Float32},
+    x::AbstractArray{Float32,2},
+    t::AbstractArray{Float32,1},
     tspan::Tuple{Number,Number}
 )
     tspan = (max(tspan[1], t[1]), max(tspan[2], t[end]))
     μ, logσ = l.vae.encoder(x)
-    z₀ = μ .+ randn(Float32, size(μ)...) .* exp.(logσ)
+    noise = randn(Float32, size(μ)...)
+    if isa(x, CUDA.CuArray)
+        noise = noise |> gpu
+    end
+    z₀ = μ .+ noise .* exp.(logσ)
     node = NeuralODE(l.dzdt, tspan)
     sol = node(z₀)
     lode_sol(t) = l.vae.decoder(convert(Vector{Float32}, sol(t)))
     return lode_sol
 end
 
-function elbo(model::LatentODE, x::Array{Float32,3}, t::Matrix{Float32})
-    batchsize = size(x)[end]
-    tspans = [(t[1, i], t[end, i]) for i in 1:size(x)[end]]
-    μ, logσ = model.vae.encoder(x)
-    z₀ = μ .+ randn(Float32, size(μ)...) .* exp.(logσ)
-    probs = [NeuralODE(model.dzdt, tspans[i]; saveat=t[:, i]) for i in 1:length(tspans)]
-    sols = [probs[i](z₀[:, i]) for i in 1:length(probs)]
-    x̂s = Flux.stack([model.vae.decoder(Flux.stack(sol.u, 2)) for sol in sols], 3)
-    logp_x_z = - sum(logitbinarycrossentropy.(x̂s, x)) / batchsize # expectation
-    kl_q_p = 0.5f0 * sum(@. (exp(logσ) + μ^2 - logσ - 1f0)) / batchsize
-    elbo = logp_x_z - model.β * kl_q_p
-    # TODO maybe add regularization (L2 reg) later
-    return -elbo
-end
 
-
-function elbo(model::LatentODE, x::Matrix{Float32}, t::Vector{Float32})
+function elbo(model::LatentODE, x::AbstractArray{Float32,2}, t::AbstractArray{Float32,1})
     tspan = (t[1], t[end])
     μ, logσ = model.vae.encoder(x)
-    z₀ = μ .+ randn(Float32, size(μ)...) .* exp.(logσ)
+    noise = randn(Float32, size(μ)...)
+    if isa(x, CUDA.CuArray)
+        noise = noise |> gpu
+    end
+    z₀ = μ .+ noise .* exp.(logσ) # TODO send this to gpu when necessary
     node = NeuralODE(model.dzdt, tspan; saveat=t)
     sol = node(z₀)
     x̂ = model.vae.decoder(Flux.stack(sol.u, 2))
--- train_lkv_latent_ode.jl
+++ train_lkv_latent_ode.jl
@@ -6,40 +6,40 @@ include("lkv.jl")
 include("latent_ode.jl")
 
 
-lode = LatentODE(2, 16, 16)
+lode = new_latent_ode(2, 16, 8)
 
 lkv_ic = [0.8, 0.4]
 true_params = [1.1, 0.4, 0.1, 0.4]
 lkv_train_samples = sample_lotka_volterra(lkv_ic, 500, 100, true_params...; train=true)
 lkv_test_samples = sample_lotka_volterra(lkv_ic, 500, 100, true_params...)
-
+nsamples = 32
 
 lkv_train_tuple = (
     Float32.(Flux.stack(
         [
-            Flux.stack(lkv_train_samples.u[i:i + 16 - 1], 2)
-            for i in 1:length(lkv_train_samples) - 16
+            Flux.stack(lkv_train_samples.u[i:i - 1 + nsamples], 2)
+            for i in 1:length(lkv_train_samples) + 1 - nsamples
         ],
         3
     )), # 2 x 16 x numsamples
     Float32.(Flux.stack(
         [
-            lkv_train_samples.t[i:i + 16 - 1]
-            for i in 1:length(lkv_train_samples.t) - 16
+            lkv_train_samples.t[i:i - 1 + nsamples]
+            for i in 1:length(lkv_train_samples.t) + 1 - nsamples
         ],
         2
-    )) # 16 x numsamples
+    ))
 )
 
-train_loader = DataLoader(lkv_train_tuple; batchsize=16, shuffle=true)
+train_loader = DataLoader(lkv_train_tuple; batchsize=8, shuffle=true)
 
 θ = Flux.params(lode.dzdt, lode.vae.encoder.rnn_network, lode.vae.decoder.network)
-opt = Descent(0.0001)
-
-for i in 1:10
+opt = ADAM(5e-5)
+# TODO log the loss each epoch
+for i in 1:100
     display("[Epoch $(i)]")
     for (x, t) in train_loader
-        display("training")
+        (x, t) = (x, t)
         grad = gradient(θ) do
             batchsize = size(t)[end]
             elbo_losses = [elbo(lode, x[:, :, i], t[:, i]) for i in 1:batchsize]
@@ -50,3 +50,15 @@ for i in 1:10
         update!(opt, θ, grad)
     end
 end
+
+lode = lode |> cpu
+
+lode_sol = lode(
+    Float32.(Flux.stack(lkv_train_samples.u, 2)),
+    Float32.(lkv_train_samples.t),
+    (0, 100)
+)
+
+plot(
+    LinRange(0, 100, 500), Flux.stack(lode_sol.(LinRange(0, 100, 500)), 1)
+)
